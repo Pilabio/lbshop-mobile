31b6a1c9584c9f9371ebb73298044ac0ec89e589
Flavio <flavio@algorich.com.br>
2019-02-01 01:21:32 -0200

    client create action and layout refactor

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,4 +8,21 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+
+  def create
+    @client = Client.new(permitted_params)
+
+    if @client.save
+      redirect_to clients_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def permitted_params
+    params.require(:client).permit(:name, :email, :phone)
+  end
 end
