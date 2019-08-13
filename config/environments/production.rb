e217aa272d6d13c95ba00ab65c24224c866762b0
Flavio <flavios.soares@hotmail.com>
2019-08-13 18:59:16 -0300

    try fix mailer error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,9 +98,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'gmail.com',
     user_name: ENV['MAILER_USERNAME'],
     password: ENV['MAILER_PASSWORD'],
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true
   }
 end
