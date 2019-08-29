2eb1c000d9df217e021c35c707671fd9dc7621bb
Flavio <flavio@algorich.com.br>
2019-08-29 17:06:56 -0300

    try fix mailer attachment

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,8 +2,16 @@ class ReportMailer < ApplicationMailer
   default from: 'brechoclosetdagirafa@gmail.com'
 
   def sales_report(client, report)
-    @report = report
-    @client = client
-    mail(to: @client.email, subject: "Relatório de vendas do mês de #{I18n.l(Date.today-1.month, format: "%B")}")
+    @report      = report.report_csv.blob.download
+    @client      = client
+    report_month = I18n.l(Date.today - 1.month, format: '%B')
+
+    attachments["relatorio-#{report_month}.csv"] = {
+      mime_type: 'text/csv',
+      content: @report
+    }
+
+    mail(to: @client.email,
+         subject: "Relatório de vendas do mês de #{report_month}")
   end
 end
